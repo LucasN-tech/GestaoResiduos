cb96f7a951fdc1d5381b7820debdb8c6c7370829
lucas.jesus <lucas.jesus@gotechnology.com.br>
2024-10-14 14:16:04 -0300

    askdjfaskdf

--- GestaoResiduos/obj/Debug/net8.0/GestaoResiduos.AssemblyInfo.cs
+++ GestaoResiduos/obj/Debug/net8.0/GestaoResiduos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestaoResiduos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91bc55aaccab3746582f9ef1a45b304eb5ed3ba3")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestaoResiduos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestaoResiduos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
